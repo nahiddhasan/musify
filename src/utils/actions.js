4568d7a382dbf5e41350f40d49f675e09a99b016
Nahid Hasan <nahidhasan1001@gmail.com>
2023-12-28 19:01:19 +0600

    find bug

--- src/utils/actions.js
+++ src/utils/actions.js
@@ -1,20 +1,20 @@
 "use server"
 
-import { headers } from "next/headers";
+// import { headers } from "next/headers";
 
-//get all playlists
-export const getPlaylist = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/playlist`, {
-      cache: "no-store",
-    method:"GET",
-    headers:headers()
-    });
-    if (!res.ok) {
-      return "Something went wrong";
-    }
-    const data = await res.json()
-    return data;
-  };
+// //get all playlists
+// export const getPlaylist = async () => {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/playlist`, {
+//     cache: "no-store",
+//     method:"GET",
+//     headers:headers(),
+//     });
+//     if (!res.ok) {
+//       return "Something went wrong";
+//     }
+//     const data = await res.json()
+//     return data;
+//   };
 
   //get singleplaylist
   export const getSinglePlaylist = async (id) => {
@@ -52,17 +52,16 @@ export const getSongs = async () => {
   return data;
 };
 
-
-  //get likedPlaylist
-  export const getLikedPlaylist = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/liked`, {
-      cache: "no-store",
-      method:"GET",
-      headers:headers()
-    });
-    if (!res.ok) {
-      return "Something went wrong";
-    }
-    const data = await res.json()
-    return data;
-  };
+  // //get likedPlaylist
+  // export const getLikedPlaylist = async () => {
+  //   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/liked`, {
+  //     cache: "no-store",
+  //     method:"GET",
+  //     headers:headers()
+  //   });
+  //   if (!res.ok) {
+  //     return "Something went wrong";
+  //   }
+  //   const data = await res.json()
+  //   return data;
+  // };
