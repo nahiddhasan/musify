54a1fc6ebbcb76473ca1010116d3901232595766
Nahid Hasan <nahidhasan1001@gmail.com>
2023-12-28 19:05:01 +0600

    rm pl

--- src/app/liked/page.jsx
+++ src/app/liked/page.jsx
@@ -1,35 +1,41 @@
-import PlaylistPlayPause from "@/components/PlaylistPlayPause";
-import { getLikedPlaylist } from "@/utils/actions";
-import { getAuthSession } from "@/utils/auth";
-import { redirect } from "next/navigation";
-import { CiCircleList } from "react-icons/ci";
-import PlayListBanner from "../playlist/_components/PlayListBanner";
-import PlayListItems from "../playlist/_components/PlayListItems";
+// import PlaylistPlayPause from "@/components/PlaylistPlayPause";
+// import { getLikedPlaylist } from "@/utils/actions";
+// import { getAuthSession } from "@/utils/auth";
+// import { redirect } from "next/navigation";
+// import { CiCircleList } from "react-icons/ci";
+// import PlayListBanner from "../playlist/_components/PlayListBanner";
+// import PlayListItems from "../playlist/_components/PlayListItems";
 
-const LikedSongs = async () => {
-  const session = await getAuthSession();
-  if (!session) {
-    return redirect("/");
-  }
-  const playlist = await getLikedPlaylist();
+// const LikedSongs = async () => {
+//   const session = await getAuthSession();
+//   if (!session) {
+//     return redirect("/");
+//   }
+//   const playlist = await getLikedPlaylist();
 
-  return (
-    <div className="p-4">
-      <PlayListBanner
-        title={playlist.title}
-        coverImage={playlist.image}
-        user={session.user}
-        type={"PlayList"}
-      />
-      <div className="flex items-center justify-between mb-4 mt-2">
-        <PlaylistPlayPause playlist={playlist} />
-        <div>
-          <CiCircleList size={30} className=" cursor-pointer" />
-        </div>
-      </div>
-      <PlayListItems playlist={playlist} />
-    </div>
-  );
+//   return (
+//     <div className="p-4">
+//       <PlayListBanner
+//         title={playlist.title}
+//         coverImage={playlist.image}
+//         user={session.user}
+//         type={"PlayList"}
+//       />
+//       <div className="flex items-center justify-between mb-4 mt-2">
+//         <PlaylistPlayPause playlist={playlist} />
+//         <div>
+//           <CiCircleList size={30} className=" cursor-pointer" />
+//         </div>
+//       </div>
+//       <PlayListItems playlist={playlist} />
+//     </div>
+//   );
+// };
+
+// export default LikedSongs;
+
+const Lk = () => {
+  return <div>Lk</div>;
 };
 
-export default LikedSongs;
+export default Lk;
--- src/app/playlist/[id]/page.jsx
+++ src/app/playlist/[id]/page.jsx
@@ -1,31 +1,37 @@
-import PlayListBanner from "@/app/playlist/_components/PlayListBanner";
-import { getPlaylist, getSinglePlaylist } from "@/utils/actions";
-import { Suspense } from "react";
-import PlayListActions from "../_components/PlayListActions";
-import PlayListItems from "../_components/PlayListItems";
+// import PlayListBanner from "@/app/playlist/_components/PlayListBanner";
+// import { getPlaylist, getSinglePlaylist } from "@/utils/actions";
+// import { Suspense } from "react";
+// import PlayListActions from "../_components/PlayListActions";
+// import PlayListItems from "../_components/PlayListItems";
 
-const PlayListPage = async ({ params }) => {
-  const { id } = params;
-  const playlist = await getSinglePlaylist(id);
-  const playlists = await getPlaylist();
-  return (
-    <div className="p-4 bg-gradient-to-t from-zinc-900 via-zinc-700/50 to-zinc-900">
-      {/* playlist banner  */}
-      <PlayListBanner
-        title={playlist.title}
-        desc={playlist.desc}
-        coverImage={playlist.image}
-        user={playlist.creator}
-        playlistId={playlist.id}
-        creatorId={playlist.creatorId}
-        type={"PlayList"}
-      />
-      <PlayListActions playlist={playlist} />
-      <Suspense fallback={<p>Loading Playlist songs...</p>}>
-        <PlayListItems playlist={playlist} playlists={playlists} />
-      </Suspense>
-    </div>
-  );
+// const PlayListPage = async ({ params }) => {
+//   const { id } = params;
+//   const playlist = await getSinglePlaylist(id);
+//   const playlists = await getPlaylist();
+//   return (
+//     <div className="p-4 bg-gradient-to-t from-zinc-900 via-zinc-700/50 to-zinc-900">
+//       {/* playlist banner  */}
+//       <PlayListBanner
+//         title={playlist.title}
+//         desc={playlist.desc}
+//         coverImage={playlist.image}
+//         user={playlist.creator}
+//         playlistId={playlist.id}
+//         creatorId={playlist.creatorId}
+//         type={"PlayList"}
+//       />
+//       <PlayListActions playlist={playlist} />
+//       <Suspense fallback={<p>Loading Playlist songs...</p>}>
+//         <PlayListItems playlist={playlist} playlists={playlists} />
+//       </Suspense>
+//     </div>
+//   );
+// };
+
+// export default PlayListPage;
+
+const Pl = () => {
+  return <div>Pl</div>;
 };
 
-export default PlayListPage;
+export default Pl;
