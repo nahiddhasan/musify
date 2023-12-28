23629083c93812d4cebbb6a8300cfe0544602f7f
Nahid Hasan <nahidhasan1001@gmail.com>
2023-12-28 19:15:33 +0600

    added pl

--- src/app/mobilelibrary/page.jsx
+++ src/app/mobilelibrary/page.jsx
@@ -1,20 +1,14 @@
-// import { getLikedPlaylist, getPlaylist } from "@/utils/actions";
-// import Library from "./_components/Library";
+import { getLikedPlaylist, getPlaylist } from "@/utils/actions";
+import Library from "./_components/Library";
 
-// const MobileLibrary = async () => {
-//   const { userPlaylists } = await getPlaylist();
-//   const likedPlaylist = await getLikedPlaylist();
-//   return (
-//     <div className="p-4">
-//       <Library likedPlaylist={likedPlaylist} playlists={userPlaylists} />
-//     </div>
-//   );
-// };
-
-// export default MobileLibrary;
-
-const Mpl = () => {
-  return <div>Mpl</div>;
+const MobileLibrary = async () => {
+  const { userPlaylists } = await getPlaylist();
+  const likedPlaylist = await getLikedPlaylist();
+  return (
+    <div className="p-4">
+      <Library likedPlaylist={likedPlaylist} playlists={userPlaylists} />
+    </div>
+  );
 };
 
-export default Mpl;
+export default MobileLibrary;
